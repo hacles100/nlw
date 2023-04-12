44864e741ca78b6ea5e5f9af36e20e2777f35af4
hacles100 <haclesrocha200@gmail.com>
2023-04-12 09:33:18 -0100

    NLW first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+function Header(){
+    return(
+        <header>
+            <div>
+             <img src="https://avatars.githubusercontent.com/u/71661551?v=4" alt="Foto do Hericles Rocha" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
